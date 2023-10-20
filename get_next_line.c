ce814900bc3035a54f0578cc054f6e97196048b4
Martib01 <martybb01@gmail.com>
2023-10-20 20:16:17 +0200

    non mi convince

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:41:52 by marboccu          #+#    #+#             */
-/*   Updated: 2023/10/20 17:33:51 by marboccu         ###   ########.fr       */
+/*   Updated: 2023/10/20 20:13:18 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static char *riempi_buffer(int fd, char *linea_letta, char *buffer)
         /* errore nel leggere il file descriptor*/
         if (bytes_letti == -1)
         {
-            free(linea_letta);
+            // free(linea_letta);
             return (NULL);
         }
         /* abbiamo letto l'intero file, quindi usciamo dal loop */
@@ -54,7 +54,7 @@ static char *riempi_buffer(int fd, char *linea_letta, char *buffer)
 }
 
 /* usa ft_substr, ft_strlen, free  */
-char *genera_linea(char *line_buffer)
+static char *genera_linea(char *line_buffer)
 {
     ssize_t i;
     char *linea_letta;
@@ -64,26 +64,64 @@ char *genera_linea(char *line_buffer)
     while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
         i++;
     /* se il char corrente o il prox è \0 non c'è next line (linea è vuota) */
-    if (line_buffer[i] == '\0' || line_buffer[1] == '\0')
-    {
-        return (NULL);
-    }
+    // if (line_buffer[i] == '\0' || line_buffer[i + 1] == '\0')
+    //     return (NULL);
     /* crea sottostringa da fine di stringa_letta a fine di line_buffer */
-    linea_letta = ft_substr(linea_letta, i + 1, ft_strlen(line_buffer) - i);
-    if (linea_letta == 0)
+    linea_letta = ft_substr(linea_letta, 0, i + 1);
+    if (!linea_letta)
     {
         free(linea_letta);
         linea_letta = NULL;
     }
-    linea_letta[i + 1] = '\0';
     return (linea_letta);
 }
+
+// static char *genera_linea_finale(char *line_buffer)
+// {
+//     ssize_t i;
+//     char *linea_letta;
+
+//     i = 0;
+//     while (linea_letta[i] != '\n' && linea_letta[i] != '\0')
+//         i++;
+//     if (linea_letta[i] == '\0' || linea_letta[i + 1] == '\0')
+//         return (NULL);
+//     linea_letta = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer));
+//     if (!linea_letta)
+//     {
+//         free(linea_letta);
+//         linea_letta = NULL;
+//     }
+//     return (linea_letta);
+// }
+
+/* crea un altra funzione simile a genera linea che usa substr per salvarsi la parte finale quindi parte da i + 1 e finisce a strlen(s)*/
 
 char *get_next_line(int fd)
 {
     static char *save;
     char *buffer;
     char *linea;
+
+    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer || fd < 0 || BUFFER_SIZE < 1)
+    {
+        free(buffer);
+        free(save);
+        buffer = NULL;
+        save = NULL;
+        return (NULL);
+    }
+    linea = riempi_buffer(fd, save, buffer);
+    free(buffer);
+    // buffer = NULL;
+    if (!linea)
+        return (NULL);
+    save = genera_linea(linea);
+    if (!save)
+        return (NULL);
+    // save = genera_linea_finale(linea);
+    return (linea);
 }
 
 int main()
@@ -92,4 +130,12 @@ int main()
     char *line;
 
     fd = open("test.txt", O_RDONLY);
+    // while ((line = get_next_line(fd)))
+    // {
+    //     printf("%s\n", line);
+    //     free(line);
+    // }
+    line = get_next_line(fd);
+    printf("%s\n", line);
+    free(line);
 }
