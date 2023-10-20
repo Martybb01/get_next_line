6d7a1ea0a6b0b9cdd07472836a4d94c17fb22742
Martib01 <martybb01@gmail.com>
2023-10-20 14:58:05 +0200

    creazioni funzioni utili + libreria

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,25 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:53:43 by marboccu          #+#    #+#             */
-/*   Updated: 2023/10/18 23:55:15 by marboccu         ###   ########.fr       */
+/*   Updated: 2023/10/20 13:47:31 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NETX_LINE_H
+#define GET_NETX_LINE_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 5
+#endif
+
+size_t ft_strlen(char *s);
+char *ft_strdup(char *s1);
+char *ft_strjoin(char *s1, char *s2);
+char *ft_substr(char *s, unsigned int start, size_t len);
+char *ft_strchr(char *s, int c);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,104 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:53:52 by marboccu          #+#    #+#             */
-/*   Updated: 2023/10/18 23:55:24 by marboccu         ###   ########.fr       */
+/*   Updated: 2023/10/20 13:44:09 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t ft_strlen(char *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+char *ft_strdup(char *s1)
+{
+    char *s2;
+    unsigned int i;
+
+    i = 0;
+    s2 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+    if (!s2)
+        return (NULL);
+    while (s1[i])
+    {
+        s2[i] = s1[i];
+        i++;
+    }
+    s2[i] = '\0';
+    return (s2);
+}
+char *ft_strjoin(char *s1, char *s2)
+{
+    int i;
+    int j;
+    char *str;
+
+    i = 0;
+    j = 0;
+    str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+    if (!str)
+        return (NULL);
+    while (s1[i])
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+    {
+        str[i] = s2[j];
+        i++;
+        j++;
+    }
+    str[i] = '\0';
+    return (str);
+}
+
+char *ft_substr(char *s, unsigned int start, size_t len)
+{
+    char *str;
+    size_t i;
+
+    i = 0;
+    if (!s)
+        return (NULL);
+    if (start > ft_strlen(s))
+        return (ft_strdup(""));
+    if (len > ft_strlen(s + start))
+        len = ft_strlen(s + start);
+    str = (char *)malloc(sizeof(char) * (len + 1));
+    if (!str)
+        return (NULL);
+    while (i < len)
+    {
+        str[i] = s[start + i];
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
+
+char *ft_strchr(char *s, int c)
+{
+    unsigned int i;
+    char carattere;
+
+    i = 0;
+    carattere = (char)c;
+
+    while (s[i])
+    {
+        if (s[i] == carattere)
+            return ((char *)s + i);
+        i++;
+    }
+    if (s[i] == carattere)
+        return ((char *)s + i);
+    return (NULL);
+}
