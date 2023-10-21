9c326be846ca6b8e94dcdf6b8a8d60d377434358
Martib01 <martybb01@gmail.com>
2023-10-21 13:10:36 +0200

    non so che faccio

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,28 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:41:52 by marboccu          #+#    #+#             */
-/*   Updated: 2023/10/20 20:13:18 by marboccu         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:54:34 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+/* crea un altra funzione simile a genera linea che usa substr per salvarsi la parte finale quindi parte da i + 1 e finisce a strlen(s)*/
+
+static char *ft_joinfree(char *buffer, char *linea_letta)
+{
+    char *temp;
+    temp = ft_strjoin(buffer, linea_letta);
+    free(buffer);
+    return (temp);
+}
+
 /* usa: read, free, ft_strdup, ft_strjoin, ft_strchr.
-    Ritorna una linea letta da un file descriptor.
-    * Se la funzione è troppo lunga, creare una custom free_strjoin richiamando dentro strjoin e free */
+    Ritorna una linea letta da un file descriptor. */
 static char *riempi_buffer(int fd, char *linea_letta, char *buffer)
 {
     ssize_t bytes_letti;
-    char *temp;
+    // char *temp;
 
     bytes_letti = 1;
     while (bytes_letti > 0)
@@ -29,7 +38,7 @@ static char *riempi_buffer(int fd, char *linea_letta, char *buffer)
         /* errore nel leggere il file descriptor*/
         if (bytes_letti == -1)
         {
-            // free(linea_letta);
+            free(linea_letta);
             return (NULL);
         }
         /* abbiamo letto l'intero file, quindi usciamo dal loop */
@@ -40,11 +49,9 @@ static char *riempi_buffer(int fd, char *linea_letta, char *buffer)
         /* condizione vera se è il primo ciclo */
         if (!linea_letta)
             linea_letta = ft_strdup(""); // alloca stringa vuota
-        temp = linea_letta;              // salva puntatore corrente in temp per poterlo liberare dopo
+        // temp = linea_letta;              // salva puntatore corrente in temp per poterlo liberare dopo
 
-        linea_letta = ft_strjoin(linea_letta, buffer);
-        free(temp);
-        temp = NULL;
+        linea_letta = ft_joinfree(linea_letta, buffer);
 
         /* è stata letta una linea completa.*/
         if (ft_strchr(buffer, '\n'))
@@ -64,38 +71,20 @@ static char *genera_linea(char *line_buffer)
     while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
         i++;
     /* se il char corrente o il prox è \0 non c'è next line (linea è vuota) */
-    // if (line_buffer[i] == '\0' || line_buffer[i + 1] == '\0')
-    //     return (NULL);
+    if (line_buffer[i] == 0 || line_buffer[i + 1] == 0)
+        return (NULL);
     /* crea sottostringa da fine di stringa_letta a fine di line_buffer */
-    linea_letta = ft_substr(linea_letta, 0, i + 1);
+    linea_letta = ft_substr(line_buffer, 0, i + 1);
     if (!linea_letta)
     {
         free(linea_letta);
         linea_letta = NULL;
     }
+    // if (line_buffer[i] || line_buffer[i] == '\n')
+    //     linea_letta[i++] = '\n';
+    linea_letta[i + 2] = '\n';
     return (linea_letta);
 }
-
-// static char *genera_linea_finale(char *line_buffer)
-// {
-//     ssize_t i;
-//     char *linea_letta;
-
-//     i = 0;
-//     while (linea_letta[i] != '\n' && linea_letta[i] != '\0')
-//         i++;
-//     if (linea_letta[i] == '\0' || linea_letta[i + 1] == '\0')
-//         return (NULL);
-//     linea_letta = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer));
-//     if (!linea_letta)
-//     {
-//         free(linea_letta);
-//         linea_letta = NULL;
-//     }
-//     return (linea_letta);
-// }
-
-/* crea un altra funzione simile a genera linea che usa substr per salvarsi la parte finale quindi parte da i + 1 e finisce a strlen(s)*/
 
 char *get_next_line(int fd)
 {
@@ -107,20 +96,17 @@ char *get_next_line(int fd)
     if (!buffer || fd < 0 || BUFFER_SIZE < 1)
     {
         free(buffer);
-        free(save);
         buffer = NULL;
-        save = NULL;
         return (NULL);
     }
     linea = riempi_buffer(fd, save, buffer);
-    free(buffer);
-    // buffer = NULL;
+    free(buffer); // non la usiamo più quindi la liberiamo per prevenire memory leaks
+    buffer = NULL;
     if (!linea)
         return (NULL);
     save = genera_linea(linea);
     if (!save)
         return (NULL);
-    // save = genera_linea_finale(linea);
     return (linea);
 }
 
@@ -128,14 +114,29 @@ int main()
 {
     int fd;
     char *line;
+    // char *line2;
+    // char *line3;
+    // char *line4;
 
     fd = open("test.txt", O_RDONLY);
-    // while ((line = get_next_line(fd)))
-    // {
-    //     printf("%s\n", line);
-    //     free(line);
-    // }
-    line = get_next_line(fd);
-    printf("%s\n", line);
-    free(line);
+    if (fd < 0)
+    {
+        perror("Error opnening file");
+        return -1;
+    }
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s\n", line);
+        free(line);
+    }
+    close(fd);
+    return (0);
+    // line = get_next_line(fd);
+    // printf("%s\n", line);
+    // line2 = get_next_line(fd);
+    // printf("%s\n", line2);
+    // line3 = get_next_line(fd);
+    // printf("%s\n", line3);
+    // line4 = get_next_line(fd);
+    // printf("%s\n", line4);
 }
